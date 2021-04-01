9ff444d51bf4db19e72bb2deb087f34e489a01b1
Yiyoujin <ujin2us@gmail.com>
2021-04-01 21:51:08 +0900

    21.04.01

--- programmers/level2/124나라의숫자.py
+++ programmers/level2/124나라의숫자.py
@@ -1,16 +1,47 @@
-import
+from itertools import product
 
-def solution(n):
-    answer = ''
+def solution(n): # 효율성 실패 
     num = [1,2,4]
 
+    if n ==1 :
+        return "1"
+    elif n == 2:
+        return "2"
+    elif n ==3:
+        return "4"
+    else:        
+        i = 9
+        li =[0,3]
+        while 1:
+            if li[-1] > 500000000:
+                break
 
+            li.append(li[-1] + i)
+            i = i * 3
+        
+        # print(len(li), li)
 
+        chk = False
+        repeat , idx = 0,0
+        for i in range(len(li)):
+            if li[i] < n <= li[i+1]:
+                chk = True
+                repeat = i+1
+                idx = i
+                break
+        
+        answer = list(product(num, repeat= repeat))
+        answer = answer[n-li[idx]-1]
+        # print(answer)
+        answer = ''.join(str(x) for x in answer)
 
-    return answer
+        return answer
 
 if __name__ == "__main__":
+    # n <= 500,000,000
     n = 1 # 1
     n = 2 # 2
     n = 3 # 4
     n = 4 # 11
+
+    print(solution(9))
